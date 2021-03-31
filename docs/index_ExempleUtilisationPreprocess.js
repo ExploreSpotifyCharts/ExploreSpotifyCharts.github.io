49df50d9b89b409dc3943dae31dfa16e8ef00672
Justine Marlow <jumart@l4712-04.info.polymtl.ca>
2021-03-31 14:42:36 -0400

    First try on preprocess for Explorer Par Artiste

--- docs/index_ExempleUtilisationPreprocess.js
+++ docs/index_ExempleUtilisationPreprocess.js
@@ -2,7 +2,8 @@
 
 //import * as d3Chromatic from 'd3-scale-chromatic'
 //import {MDCRipple} from '@material/ripple';
-import * as preprocess from './scripts/preprocess_ParPays.js'
+import * as preprocess_ParPays from './scripts/preprocess_ParPays.js'
+import * as preprocess_ParArtiste from './scripts/preprocess_ParArtiste.js'
 
 /**
  * @file This file is the entry-point for the the code for Team 3 project for the course INF8808.
@@ -35,13 +36,18 @@ import * as preprocess from './scripts/preprocess_ParPays.js'
   ]
   */
 
- const country = 'ca'
+ const country = 'fr'
  d3.csv('./assets/data/'+country+'.csv', d3.autoType).then(function (data) {
-    const data_preprocessed_track = preprocess.ExplorerParPays_Track(data, new Date('2017-01-01'), new Date('2020-04-20'))
-    console.log(data_preprocessed_track)
+    const data_preprocessed_countrytrack = preprocess_ParPays.ExplorerParPays_Track(data, new Date('2017-01-01'), new Date('2020-04-20'))
+    console.log(data_preprocessed_countrytrack)
     //here we can continue with the data -> viz
 
-    const data_preprocessed_artist = preprocess.ExplorerParPays_Artist(data, new Date('2017-01-01'), new Date('2020-04-20'))
+    const data_preprocessed_countryartist = preprocess_ParPays.ExplorerParPays_Artist(data, new Date('2017-01-01'), new Date('2020-04-20'))
+    console.log(data_preprocessed_countryartist)
+    //here we can continue with the data -> viz
+
+    const artiste = 'Orelsan'
+    const data_preprocessed_artist = preprocess_ParArtiste.ExplorerParArtiste(data, artiste, new Date('2017-01-01'), new Date('2020-04-20'))
     console.log(data_preprocessed_artist)
     //here we can continue with the data -> viz
  })
