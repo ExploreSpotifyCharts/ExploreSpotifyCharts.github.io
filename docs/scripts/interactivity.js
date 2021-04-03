a36a379a1db49ce3a96b9127aa39898e52e7c497
Tanguy97 <grogros04@gmail.com>
2021-04-02 20:47:37 -0400

    add suggestbox

--- docs/scripts/interactivity.js
+++ docs/scripts/interactivity.js
@@ -1,41 +1,70 @@
-/*Change viz and menu color on click*/
+/* Private function*/
+
+/**
+ * Navigate to the selected tab
+ * @param {*} element 
+ */
 function navigate(element) {
-  console.log(element.innerText)
+  const tab = element.innerText
   d3.selectAll('li').attr('class', null)
   d3.select(element).attr('class', 'selected')
+  resetForm()
+  createForm(tab)
 }
 
+/**
+ * Create the header form link to the tab
+ * @param {String} tab 
+ */
+function createForm(tab) {
+  //Mock data for the test
+    const countries = ['France','USA','Finlande','Test1', 'Test2']
+    const artists = ['Ed Sheran','Angèle','Lompale','Roméo Elvis','Therapie TAXI']
+    const titles = []
 
-function createForm() {
-    const data = ['','France','USA','Finlande','Test1', 'Test2']
-    createSuggestbox('Pays', data)
+    switch(tab) {
+      case "Pays":
+      case "Tendances":
+        createSuggestboxe('Pays', countries)
+        break
+      case "Artiste":
+        createSuggestboxe('Artiste', artists)
+        createSuggestboxe('Pays', countries)
+        break
+      case "Titre":
+        createSuggestboxe('Titre', titles)
+        break
+    }
+
+    createDatePickers()  
 }
 
-function destroyFrom() {
-
-}
-
-function refresh(element) {
-  element.nextElementSibling.value = element.value
+/**
+ * Empty the form
+ */
+function resetForm() {
+  d3.selectAll('.suggestboxe').remove()
 }
 
 /**
  * Create a suggestbox tag with the given label
  * @param {string} label 
  */
-function createSuggestbox(label,data) {
-  const form = d3.select('form')
+function createSuggestboxe(label,data) {
+  const suggestboxe = d3.select('form .suggestboxes').append('div').attr('class','suggestboxe')
 
-  form.append('label')
+  suggestboxe
+  .append('label')
   .attr('for', label)
   .text(label + ":")
 
-  form.append('input')
+  suggestboxe
+  .append('input')
   .attr('list', "list" + label)
-  .attr('name', label.slice(0,label.length))
+  .attr('name', label)
   .attr('id', label)
 
-  form
+  suggestboxe
   .append('datalist')
   .attr('id', "list" + label)
   .selectAll('option')
@@ -43,6 +72,12 @@ function createSuggestbox(label,data) {
   .append('option')
   .attr('value', d => d)
 }
+
+function createDatePickers(){
+  
+}
+
+/*Public function*/
 
 /* Initialize view element */
 export function initialize() {
