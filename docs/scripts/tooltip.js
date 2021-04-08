abae70330e95d6639c6fedae6b0a76856d924776
Justine Marlow <jumart@l4712-04.info.polymtl.ca>
2021-04-08 15:06:35 -0400

    Tooltip V2 : text color change + small triangle as pointer

--- docs/scripts/tooltip.js
+++ docs/scripts/tooltip.js
@@ -4,9 +4,24 @@
  * @param {object} d The data associated to the hovered element
  * @returns {string} The tooltip contents
  */
-export function getContents (d) {
+export function getContents_Streams (d) {
   // Generate tooltip contents
-  let tooltip_string = '<span class="tooltip-value">'
+  let tooltip_string = '<span class="tooltip-value-streams">'
+  tooltip_string += '<strong> Date : </strong> '+ d["Date"] +'<br>'
+  tooltip_string += '<strong> Streams : </strong> '+d["Streams"]
+  tooltip_string += '</span>'
+  return tooltip_string
+}
+
+/**
+ * Defines the contents of the tooltip. See CSS for tooltip styling.
+ *
+ * @param {object} d The data associated to the hovered element
+ * @returns {string} The tooltip contents
+ */
+export function getContents_Total (d) {
+  // Generate tooltip contents
+  let tooltip_string = '<span class="tooltip-value-total">'
   tooltip_string += '<strong> Date : </strong> '+ d["Date"] +'<br>'
   tooltip_string += '<strong> Streams : </strong> '+d["Streams"]
   tooltip_string += '</span>'
