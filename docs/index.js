0526b68183982cf6ffc657de74ff900b02072a23
Justine Marlow <jumart@l4712-04.info.polymtl.ca>
2021-04-14 21:32:39 -0400

    new version for generate all extra files needed

--- docs/index.js
+++ docs/index.js
@@ -17,7 +17,7 @@ import * as preprocess_ParTendance from './scripts/preprocess_ParTendance.js'
 
 (function (d3) {
 
-  let countries = [
+  /*let countries = [
     'global',
     'ar', 'at', 'au',
     'be', 'bo', 'br',
@@ -37,36 +37,27 @@ import * as preprocess_ParTendance from './scripts/preprocess_ParTendance.js'
     'tr', 'tw',
     'us', 'uy'
   ]
+  */
 
 
 const PATH = './assets/data/'
-//let countries = ['global', 'be', 'ca', 'fr'] //à remplacer à terme par la liste complètes des country code (cf plus haut)
+let countries = ['global', 'be', 'ca', 'fr'] //à remplacer à terme par la liste complètes des country code (cf plus haut)
 let call_countries = []
 
-//DOWNLOAD track_countries.csv
+//DOWNLOAD tracks_data.csv
 /*
 countries.forEach(country => call_countries.push(d3.csv(PATH+country+'.csv', preprocess_Helpers.SpotifyDataParser).then(function (data) {
   let tracks = [...new Set(data.map(line => {
-    if (line && line['Track Name'])
+    if (line && line['Track Name'] && line['Artist'] && line['spotify_id'])
     {
       let track = line['Track Name']
-      while(track.includes('#'))
+      let artist = line['Artist']
+      if (track != '' && track != 'NA' && artist != '' && artist != 'NA')
       {
-        track = track.replace('#','')
+        return artist+','+track
       }
-      while(track.includes(','))
-      {
-        track = track.replace(',','')
-      }
-      return track
     }
   }))].sort()
-  
-  let index_to_remove = tracks.indexOf('')
-  if (index_to_remove > -1) { tracks.splice(index_to_remove, 1)}
-  index_to_remove = tracks.indexOf('NA')
-  if (index_to_remove > -1) { tracks.splice(index_to_remove, 1)}
-
   console.log(tracks)
   return tracks
 })))
@@ -89,13 +80,13 @@ Promise.all(call_countries)
     console.log(data_preprocessed)
 
 
-    let csvContent = "data:text/csv;charset=utf-8,"+"Track Name,Countries"+"\n"
+    let csvContent = "data:text/csv;charset=utf-8,"+"Artist,Track Name,Countries"+"\n"
       + data_preprocessed.map(e => e[0]+','+e[1].join("|")).join("\n");
-    //console.log(csvContent)
+    console.log(csvContent)
     var encodedUri = encodeURI(csvContent)
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "track_countries.csv");
+    link.setAttribute("download", "tracks_data.csv");
     document.body.appendChild(link); // Required for FF
     link.click(); // This will download the data file named "my_data.csv".
 
@@ -104,32 +95,18 @@ Promise.all(call_countries)
     // handle error here
     console.log(err)
   })
-  */
+*/
 
-//DOWNLOAD artist_countries.csv
+//DOWNLOAD artists_countries.csv
 /*
 countries.forEach(country => call_countries.push(d3.csv(PATH+country+'.csv', preprocess_Helpers.SpotifyDataParser).then(function (data) {
   console.log(country)
   let artists = [...new Set(data.map(line => {
-    if (line && line['Artist'])
+    if (line && line['Artist'] && line['Artist'] != '' && line['Artist'] != 'NA')
     {
-      let artist = line['Artist']
-      while(artist.includes('#'))
-      {
-        artist = artist.replace('#','')
-      }
-      while(artist.includes(','))
-      {
-        artist = artist.replace(',','')
-      }
-      return artist
+      return line['Artist']
     }
   }))].sort()
-  
-  let index_to_remove = artists.indexOf('')
-  if (index_to_remove > -1) { artists.splice(index_to_remove, 1)}
-  index_to_remove = artists.indexOf('NA')
-  if (index_to_remove > -1) { artists.splice(index_to_remove, 1)}
 
   console.log(artists)
   return artists
@@ -159,7 +136,7 @@ Promise.all(call_countries)
     var encodedUri = encodeURI(csvContent)
     var link = document.createElement("a");
     link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "artist_countries.csv");
+    link.setAttribute("download", "artists_countries.csv");
     document.body.appendChild(link); // Required for FF
     link.click(); // This will download the data file named "my_data.csv".
 
@@ -170,66 +147,12 @@ Promise.all(call_countries)
   })
   */
 
-  //DOWNLOAD titres.csv
+  //DOWNLOAD artists_global.csv
   /*
-  countries.forEach(country => call_countries.push(d3.csv(PATH+country+'.csv', preprocess_Helpers.SpotifyDataParser).then(function (data) {
-      let data_preprocessed = [...new Set(data.map(line => 
-        {
-          if(line && line['Artist'] && line['Track Name']){
-            let artist = line['Artist'].replace('#', '')
-            let track = line['Track Name']
-            while (track.includes('#'))
-            {
-              track = track.replace('#', '')
-            }
-            while (track.includes(','))
-            {
-              track = track.replace(',', '')
-            }
-            while (artist.includes('#'))
-            {
-              artist = artist.replace('#', '')
-            }
-            while (artist.includes(','))
-            {
-              artist = artist.replace(',', '')
-            }
-            if (artist != '' && artist != 'NA' && track != '' && track != 'NA') {return artist+','+track}
-            }
-          }))].sort()
-        console.log(data_preprocessed)
-        return data_preprocessed
-    })))
-
-Promise.all(call_countries)
-  .then(function(files) {
-    let data_preprocessed = []
-    files.forEach(file => {
-      data_preprocessed = data_preprocessed.concat(file)
-      data_preprocessed = [...new Set(data_preprocessed)].sort()
-    })
-    console.log(data_preprocessed)
-
-
-    let csvContent = "data:text/csv;charset=utf-8,"+"Artist,Track Name"+"\n"
-    + data_preprocessed.join("\n")
-    //console.log(csvContent)
-    var encodedUri = encodeURI(csvContent)
-    var link = document.createElement("a");
-    link.setAttribute("href", encodedUri);
-    link.setAttribute("download", "titres.csv");
-    document.body.appendChild(link); // Required for FF
-    link.click(); // This will download the data file named "my_data.csv".
-
-  })
-    .catch(function(err) {
-    // handle error here
-    console.log(err)
-  })
-  */
-
   d3.csv(PATH+'global'+'.csv', preprocess_Helpers.SpotifyDataParser).then(function (data) {
-    let data_preprocessed = [...new Set(data.map(line => {if (line && line['Artist']) return line['Artist']}))].sort()
+    let data_preprocessed = [...new Set(data.map(line => {
+      if (line && line['Artist'] && line['Artist'] != '' && line['Artist'] != 'NA')
+      return line['Artist']}))].sort()
     console.log(data_preprocessed)
 
     let csvContent = "data:text/csv;charset=utf-8,"+"Artist"+"\n"
@@ -242,5 +165,6 @@ Promise.all(call_countries)
     document.body.appendChild(link); // Required for FF
     link.click(); // This will download the data file named "my_data.csv".
   })
+  */
   
 })(d3)
